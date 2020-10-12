5f4b7182a553947e87f880405fd90744058b3f2a
Susan Shang <ys3386@columbia.edu>
2020-10-12 03:22:22 +0000

    Modify views.py

--- project/squirrel/views.py
+++ project/squirrel/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse,HttpResponseRedirect
+from .models import Sighting
 
-# Create your views here.
+def map(request):
+    sightings = Sighting.objects.all()
+    context = {
+            'sightings':sightings,
+            }
+    return render(request,'squirrel/map.html',context)
