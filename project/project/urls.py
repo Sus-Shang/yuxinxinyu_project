b9db63ec39045f5aa355b3c39796666b17bf9c85
Xinyu Mei <xm2256@columbia.edu>
2020-10-15 03:37:56 +0000

    Change path to under sightings

--- project/project/urls.py
+++ project/project/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from squirrel import views
 
 urlpatterns = [ 
     path('admin/', admin.site.urls),
-    path('squirrel/', include('squirrel.urls'))
+    path('sightings/add', views.add,name = 'add')
 ]
 
