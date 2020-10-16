843cd93df22897710c202e9d7e148175e01fd6b2
Xinyu Mei <xm2256@columbia.edu>
2020-10-16 03:53:46 +0000

    Modified update views.

--- project/squirrel/urls.py
+++ project/squirrel/urls.py
@@ -10,4 +10,5 @@ urlpatterns=[
         path('sightings/<unique_squirrel_ID>/IDdetails', views.IDdetails, name='IDdetails'),
         path('sightings/add/', views.add, name='add'),
         path('sightings/stats/',views.stats, name='stats')
+        path('sightings/<unique_squirrel_ID>/',views.update, name='update')
         ]
