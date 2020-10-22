0ec8e59ad127e14d329ba69817729b9dea8cce21
Xinyu Mei <xm2256@columbia.edu>
2020-10-22 04:20:19 +0000

    Test the website

--- project/squirrel/forms.py
+++
@@ -1,9 +0,0 @@
-
-from django.forms import ModelForm
-from squirrel.models import Sighting
-
-
-class SightingForm(ModelForm):
-    class Meta:
-        model = Sighting
-        fields = '__all__'
--- project/urls.py
+++
@@ -1,7 +0,0 @@
-
-from django.contrib import admin
-from django.urls import path,includeurl
-patterns = [ 
-path('', include('squirrel.urls')), 
-path('admin/',admin.site.urls) ]
-
