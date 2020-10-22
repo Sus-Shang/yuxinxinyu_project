b39fdba001a7cbdf3d508340f12a95de784c232c
Susan Shang <ys3386@columbia.edu>
2020-10-22 14:13:30 +0000

    Set up venv again and modify urls

--- project/squirrel/urls.py
+++ project/squirrel/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 app_name = 'squirrel'
 urlpatterns=[ 
@@ -7,7 +7,7 @@ path('sightings',views.sightings,name = 'sightings'),
 path('map/', views.map, name='map'), 
 path('sightings/add/', views.add, name='add'), 
 path('sightings/stats/',views.stats, name='stats'), 
-path('sightings/<unique_squirrel_ID>/', views.update, name='update'), 
-path('sightings/<unique_squirrel_ID>/IDdetails', views.update, 
+re_path(r'sightings/(?P<unique_squirrel_ID>[0-9]+[A-Z]-[A-Z]{2}-[0-9]{4}-[0-9]{2})/$', views.update, name='update'),
+re_path(r'sightings/(?P<unique_squirrel_ID>[0-9]+[A-Z]-[A-Z]{2}-[0-9]{4}-[0-9]{2})/IDdetails', views.update, 
 name='details') ]
 
